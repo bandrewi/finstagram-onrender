908ad34ed550283d626851a9fa556479180dd849
andrew bui <bandrewi98@gmail.com>
2022-11-28 01:57:33 -0800

    configured comment model file for schema

--- app/models/comment.py
+++ app/models/comment.py
@@ -4,10 +4,13 @@ from .user import User
 class Comment(db.Model):
     __tablename__ = 'comments'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     photo_id = db.Column(db.Integer, db.ForeignKey(
         "photos.id"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
     body = db.Column(db.String(255))
 
     def to_dict(self):
